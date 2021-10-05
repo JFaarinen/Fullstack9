c87ba1a9c6e2a7a2d4922ecb0a00db48382f53ce
Juho F <juho.faarinen@gmail.com>
2021-10-05 23:28:34 +0300

    Teht 9.15

--- course-information/src/components/Content.tsx
+++ course-information/src/components/Content.tsx
@@ -1,13 +1,96 @@
 import React from 'react';
-import { CourseParts } from '../types';
+import { CoursePart } from '../types';
 
-const Content: React.FC<{ courseParts: CourseParts[] }> = ({ courseParts }) => {
+const Course: React.FC<{ course: CoursePart }> = ({ course }) => {
+    switch (course.type) {
+        case "normal":
+            return (
+                <div>
+                    <div>
+                        <strong>
+                            {course.name} {course.exerciseCount}
+                        </strong>
+                    </div>
+                    <div>
+                        <em>
+                            {course.description}
+                        </em>
+                    </div>
+                    <br />
+                </div>
+            );
+        case "groupProject":
+            return (
+                <div>
+                    <div>
+                        <strong>
+                            {course.name} {course.exerciseCount}
+                        </strong>
+                    </div>
+                    <div>
+                        project exercises {course.groupProjectCount}
+                    </div>
+                    <br />
+                </div>
+            );
+        case "submission":
+            return (
+                <div>
+                    <div>
+                        <strong>
+                            {course.name} {course.exerciseCount}
+                        </strong>
+                    </div>
+                    <div>
+                        <em>
+                            {course.description}
+                        </em>
+                    </div>
+                    <div>
+                        submit to <a href={course.exerciseSubmissionLink}>
+                            {course.exerciseSubmissionLink}
+                        </a>
+                    </div>
+                    <br />
+                </div>
+            );
+        case "special":
+            return (
+                <div>
+                    <div>
+                        <strong>
+                            {course.name} {course.exerciseCount}
+                        </strong>
+                    </div>
+                    <div>
+                        <em>
+                            {course.description}
+                        </em>
+                    </div>
+                    <div>
+                        required skills: {course.requirements.map(r => `${r} `)}
+                    </div>
+                    <br />
+                </div>
+            );
+        default:
+            return assertNever(course);
+    }
+};
+
+const assertNever = (value: never): never => {
+    throw new Error(
+        `Unhandled discriminated union member: ${JSON.stringify(value)}`
+    );
+};
+
+
+const Content: React.FC<{ courseParts: CoursePart[] }> = ({ courseParts }) => {
     return (
         <div>
             {
-                courseParts.map(p =>
-                    <div key={p.name}>{p.name} {p.exerciseCount} </div>)
-            }
+                courseParts.map(p => <Course key={p.name} course={p} />
+                )}
         </div>
     );
 };
