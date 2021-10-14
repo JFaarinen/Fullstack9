507fc3251e1701b20a968ff2345948141d2bb43f
Juho F <juho.faarinen@gmail.com>
2021-10-14 20:35:21 +0300

    Teht 9.19

--- patientor-back/src/types.ts
+++ patientor-back/src/types.ts
@@ -1,4 +1,4 @@
-export interface Diagnose {
+export interface Diagnosis {
     code: string;
     name: string;
     latin?: string;
@@ -20,11 +20,47 @@ export enum Gender {
     Other = "other"
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
+interface BaseEntry {
+    id: string;
+    description: string;
+    date: string;
+    specialist: string;
+    diagnosisCodes?: Array<Diagnosis['code']>;
+}
 
+interface HealthCheckEntry extends BaseEntry {
+    type: "HealthCheck";
+    healthCheckRating: HealthCheckRating;
+}
+
+export enum HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3
+}
+
+interface HospitalEntry extends BaseEntry {
+    type: "Hospital";
+    discharge: Discharge;
+}
+
+interface Discharge {
+    date: string;
+    criteria: string;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+    type: "OccupationalHealthcare";
+    employerName: string;
+    sickLeave?: SickLeave;
+}
+
+interface SickLeave {
+    startDate: string;
+    endDate: string;
 }
 
 export type SecurePatient = Omit<Patient, "ssn" | "entries">;
 export type NewPatient = Omit<Patient, "id">;
-
+export type Entry = | HospitalEntry | OccupationalHealthcareEntry | HealthCheckEntry;
